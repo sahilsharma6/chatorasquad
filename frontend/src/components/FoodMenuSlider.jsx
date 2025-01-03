7302ef47086eb37788895ac5a2a4fc42d0216880
Rudra-Maity <maityrudra228@gmail.com>
2025-01-03 12:21:59 +0530

    Added OrderDetails Page

--- frontend/src/components/FoodMenuSlider.jsx
+++ frontend/src/components/FoodMenuSlider.jsx
@@ -23,13 +23,16 @@ const FoodMenuSwiper = () => {
   const containerVariants = {
     animate: {
       x: isHovered ? "0%" : ["0%", "-100%"],
+      
       transition: {
         x: {
           repeat: Infinity,
-          duration: 12,
+          
+          duration: foodItems.length ? foodItems.length * 2 : 12,
           ease: "linear",
         },
       },
+      
     },
   };
   console.log(foodItems);
@@ -69,6 +72,42 @@ const FoodMenuSwiper = () => {
           variants={containerVariants}
           animate="animate"
         >
+          {[...foodItems, ...foodItems].map((item, index) => (
+            <Link to={`/menu/details/${item._id}`} key={index}>
+              <div
+                key={index}
+                className="w-72 bg-yellow-300 rounded-lg shadow-xl flex-shrink-0 p-4 h-96"
+              >
+                <div className="relative">
+                  <div className="w-full h-48 rounded-full overflow-hidden mb-4">
+                    <img
+                      src={
+                        item.image ||
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg4HQZUwt86vrz_zqFyfLKsIkV0ZkfQoCooA&s"
+                      }
+                      alt={item.name}
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+                  <div className="absolute -top-2 -right-2 w-20 h-20 bg-gray-900 rounded-full gap-0 items-center justify-center text-white flex flex-wrap">
+                    <IndianRupee />
+                    <span className="text-white font-bold">
+                      {" "}
+                      {item?.sellingPrice}
+                      {/* ferf */}
+                    </span>
+                  </div>
+                </div>
+                <h3 className="text-xl font-bold text-center text-gray-900">
+                  {item.name}
+                </h3>
+                <p className="text-gray-900 text-center text-sm">
+                  {console.log(foodItems)}
+                  {item.description.slice(0, 150)}...
+                </p>
+              </div>
+            </Link>
+          ))}
           {[...foodItems, ...foodItems].map((item, index) => (
             <Link to={`/menu/details/${item._id}`} key={index}>
               <div
