fd19cef9536c26a9bdfec4a0d9079de948d5bf8e
Rudra-Maity <maityrudra228@gmail.com>
2025-02-12 21:03:22 +0530

    added add rom

--- backend/middlewares/checkAdmin.js
+++ backend/middlewares/checkAdmin.js
@@ -1,6 +1,6 @@
 
 const checkAdmin = (req, res, next) => {
-  if (req.user.role !== "Admin") {
+  if (req.user.role !== "admin") {
     return res.status(401).json({ message: "Unauthorized" });
   }
   next();
